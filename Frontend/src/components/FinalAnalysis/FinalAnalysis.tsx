28a3cb4503b1abd8bdfcb5966d3994d2dbbd97be
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-05-14 10:41:16 -0300

    Add card glow while its not flipped

--- Frontend/src/components/FinalAnalysis/FinalAnalysis.tsx
+++ Frontend/src/components/FinalAnalysis/FinalAnalysis.tsx
@@ -18,21 +18,30 @@ const FinalAnalysis: FC<FinalAnalysisProps> = ({ infos }) => {
   const [flipped, setFlipped] = useState(false);
 
   return (
-    <div className="perspective-[1000px] w-full flex flex-col items-center  justify-center mt-20 mb-80">
+    <div className="perspective-[1000px] w-full flex flex-col items-center pt-10  justify-center mt-20 mb-80">
       <motion.div
-        className="relative w-[500px] h-[500px]"
-        animate={{ rotateY: flipped ? 180 : 0 }}
-        transition={{ duration: 0.8 }}
+        className="relative w-[450px] h-[683px] cursor-pointer rounded-2xl"
+        animate={{
+          rotateY: flipped ? 180 : 0,
+          boxShadow: flipped
+            ? "0px 0px 0px rgba(255, 215, 0, 0)" // sem glow depois de virar
+            : ["0px 0px 0px rgba(255, 215, 0, 0)", "0px 0px 20px rgba(255, 215, 0, 0.4)", "0px 0px 0px rgba(255, 215, 0, 0)"]
+        }}
+        transition={{
+          duration: 2,
+          repeat: flipped ? 0 : Infinity,
+          ease: "easeInOut"
+        }}
         style={{ transformStyle: 'preserve-3d' }}
-        onClick={() => setFlipped(!flipped)}
+        onClick={() => setFlipped(true)}
       >
         {/* VERSO */}
-        <div className="absolute w-full h-full backface-hidden rounded-xl text-white transform p-6">
+        <div className={`absolute w-full h-full backface-hidden rounded-xl text-white transform  `}>
             <CardBack />
         </div>
 
         {/* FRENTE */}
-        <div className="absolute w-full h-full backface-hidden rounded-xl text-white transform p-6" style={{ transform: 'rotateY(180deg)' }}>
+        <div className="absolute w-full h-full backface-hidden rounded-xl text-white transform " style={{ transform: 'rotateY(180deg)' }}>
           <Card infos={infos} />
         </div>
       </ motion.div>
