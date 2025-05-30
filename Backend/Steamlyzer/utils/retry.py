613ef81d92beec03636b20d3cf7c87c7f2a55692
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-05-30 18:58:59 -0300

    :bulb: docs: add inline comments to improve code readability

--- Backend/Steamlyzer/utils/retry.py
+++ Backend/Steamlyzer/utils/retry.py
@@ -1,6 +1,7 @@
 import time, requests
 
 def make_request_with_retry(url, retries=5, delay=1):
+    """Faz uma requisição GET e tenta novamente se a resposta for diferente de 200."""
     for attempt in range(retries):
         resp = requests.get(url)
         if resp.status_code == 200:
