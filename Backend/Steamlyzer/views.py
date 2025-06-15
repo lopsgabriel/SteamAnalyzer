630d48ed2f9796420b0b4af4365d5dd73efee777
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-06-15 18:26:29 -0300

    :goal_net: fix: improve error handling for Steam API responses

--- Backend/Steamlyzer/views.py
+++ Backend/Steamlyzer/views.py
@@ -15,7 +15,6 @@ from Steamlyzer.services.ai_responses import ai_responses
 from Steamlyzer.utils.retry import make_request_with_retry
 from Steamlyzer.utils.constants import STEAM_API_KEY
 nest_asyncio.apply()
-
 class SteamAnalyzerViewSet(ViewSet):
 
     @action(detail=False, methods=["get"])
@@ -29,21 +28,31 @@ class SteamAnalyzerViewSet(ViewSet):
             steam_id = verify_steamid_or_vanity_url(steam_id)
             if not steam_id:
                 return Response({"error": "Usuario não encontrado. Tente novamente."}, status=400)
+            
+            try:
+                # Executa o async
+                user_profile_data = asyncio.run(gather_user_profile(steam_id))
+                if not isinstance(user_profile_data, dict):
+                    print("user_profile_data inválido:", user_profile_data)
+                    return Response({"error": "Erro ao obter perfil do usuário."}, status=500)
 
-            # Executa o async
-            user_profile_data = asyncio.run(gather_user_profile(steam_id))
-            # Se for um erro, retorna direto
-            if isinstance(user_profile_data, Response):
-                return user_profile_data  # ← aqui retorna "Perfil privado" com status 403, por exemplo
-            user_data = user_profile_data.get("response", {}).get("players", [{}])[0]
-    
+                user_data = user_profile_data.get("response", {}).get("players", [{}])[0]
+            except Exception as e:
+                print("Erro ao buscar dados da Steam:", e)
+                return Response({"error": "Erro ao buscar dados da Steam 2"}, status=500)
+
             #busca jogos do usuario pela steamID          
             url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={STEAM_API_KEY}&steamid={steam_id}&include_appinfo=1&include_played_free_games=1"
             response = requests.get(url)
 
+            #Antes quando o response fosse diferente de 200, e o retry desse erro, dava attributeerror: NoneType, corrigi com uma logica para identificar corretamente quando deu erro
             if response.status_code != 200:
-                response = make_request_with_retry(url)
-                print("Erro ao chamar API da Steam:", response.status_code, response.text)
+                retried_response = make_request_with_retry(url)
+                if retried_response is None or retried_response.status_code != 200:
+                    print("Erro ao chamar API da Steam:", retried_response.status_code if retried_response else "None", 
+                        retried_response.text if retried_response else "Sem resposta")
+                    return Response({"error": "Erro ao buscar jogos da Steam"}, status=500)
+                response = retried_response
 
         except Exception:
             return Response({"error": "Erro ao buscar dados da Steam"}, status=500)
@@ -100,7 +109,7 @@ class SteamAnalyzerViewSet(ViewSet):
         genres_hours = dict(sorted(genres_hours.items(), key=lambda item: item[1], reverse=True)[:6])
         categories_hours = dict(sorted(categories_hours.items(), key=lambda item: item[1], reverse=True)[:6])
         player_type = define_player_type(genres_hours, categories_hours)
-        time_played = round((sum(int(game["playtime_forever"]) for game in games if isinstance(game, dict) and "playtime_forever" in game) / 60), 2),
+        time_played = round((sum(int(game["playtime_forever"]) for game in games if isinstance(game, dict) and "playtime_forever" in game) / 60), 2)
         ai_responses_data = ai_responses(
                 user_data.get("personaname", "Unknown"), (created_at.strftime("%d-%m-%Y")), len(games), time_played, genres_hours, categories_hours, top_5_games, days_since_creation)
         result = {
